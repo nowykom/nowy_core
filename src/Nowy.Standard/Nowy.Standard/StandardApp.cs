8da9711309abd97f9685d3585345a23b16dce60c
Tobias Schulz <tobias.schulz@linux.com>
2023-04-10 04:22:21 +0200

    Update StandardApp.cs

--- src/Nowy.Standard/Nowy.Standard/StandardApp.cs
+++ src/Nowy.Standard/Nowy.Standard/StandardApp.cs
@@ -5,8 +5,4 @@ namespace Nowy.Standard;
 public static class StandardApp
 {
     public static IServiceProvider Services { get; set; } = null!;
-
-    public static string AppVersion { get; set; } = "0";
-    public static string AppName { get; set; } = "unknown-app";
-    public static string AppTitle { get; set; } = "Unknown App";
 }
