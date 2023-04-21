b52fd65d6a55aee2c3e855fb93c68d1ab7f3d566
Tobias Schulz <tobias.schulz@linux.com>
2023-04-21 23:12:54 +0200

    .

---
+++ src/Nowy.Standard/Nowy.Standard/ServiceCollectionHostedServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Nowy.Standard;
+
+public static class ServiceCollectionHostedServiceExtensions
+{
+    public static void AddHostedService<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, IHostedService, TService
+    {
+        services.AddSingleton<TService, TImplementation>();
+        services.AddHostedService<HostedServiceWrapper<TService>>();
+    }
+
+    private class HostedServiceWrapper<TService> : IHostedService
+    {
+        private readonly IHostedService _hostedService;
+
+        public HostedServiceWrapper(TService hostedService)
+        {
+            this._hostedService = (IHostedService)hostedService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return this._hostedService.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return this._hostedService.StopAsync(cancellationToken);
+        }
+    }
+}
