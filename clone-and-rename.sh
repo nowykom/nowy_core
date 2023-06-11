5e89e9ab48d65a2d9752ed98d28697074f1db6df
Tobias Schulz <tobias.schulz@linux.com>
2023-06-11 03:47:42 +0200

    .

--- clone-and-rename.sh
+++ clone-and-rename.sh
@@ -17,7 +17,7 @@ gsed -i 's@nowy@lr@gm' .github/workflows/deploy-nuget.yml .nuke/parameters.json
 
 ../code-rename/coderename.pl "Nowy" "LR"
 
-gsed -i 's@lr/_packaging/LR/nuget/v3/index.json@nowy/_packaging/Nowy/nuget/v3/index.json@gm' nuget.config nukebuild/Build.cs
+gsed -i 's@lr/_packaging/LR/nuget/v3/index.json@leuchtraketen/_packaging/Leuchtraketen1/nuget/v3/index.json@gm' nuget.config nukebuild/Build.cs
 
 git add --all
 
